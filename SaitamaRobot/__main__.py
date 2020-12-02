2a5cadb056aa1ea64a66b5dff700f2464df76b34
Sasuke Uchiha <73424435+FtSasaki@users.noreply.github.com>
2020-12-02 20:36:17 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -222,13 +222,11 @@ def start(update: Update, context: CallbackContext):
                             text="⚡️ Help ⚡️",
                             callback_data="help_back")
                     ]]))
-            
-  else:
-      update.effective_message.reply_text(
-        "I'm awake already!\n<b>Haven't slept since:</b> <code>{}</code>"
-         .format(uptime),
-         parse_mode=ParseMode.HTML)
-
+    else:
+        update.effective_message.reply_text(
+            "I'm awake already!\n<b>Haven't slept since:</b> <code>{}</code>"
+            .format(uptime),
+            parse_mode=ParseMode.HTML)
 
 # for test purposes
 def error_callback(update: Update, context: CallbackContext):
