f38a93b7f0c1ae41721b3e4cac03b4f5c12ea2f1
Sasuke Uchiha <73424435+FtSasaki@users.noreply.github.com>
2020-12-02 19:21:41 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -59,7 +59,7 @@ You can find my list of available commands with /help.
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
-I'm a Hero For Fun and help admins manage their groups with One Punch! Have a look at the following for an idea of some of \
+I'm Dr Stone and I help admins to manage their groups with my powers! Have a look at the following for an idea of some of \
 the things I can help you with.
 
 *Main* commands available:
