fc2037f28f2e67ca641953e607f16302e55c3fdb
Sasuke Uchiha <73424435+FtSasaki@users.noreply.github.com>
2020-12-02 20:37:53 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -221,7 +221,7 @@ def start(update: Update, context: CallbackContext):
                          InlineKeyboardButton(
                             text="⚡️ Help ⚡️",
                             callback_data="help_back")
-                    ]]))
+                     ]]))
     else:
         update.effective_message.reply_text(
             "I'm awake already!\n<b>Haven't slept since:</b> <code>{}</code>"
