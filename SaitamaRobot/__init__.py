1b62e94dd218515ad3e6ace4e92e12d4cbbe15ad
Sasuke Uchiha <73424435+FtSasaki@users.noreply.github.com>
2020-12-08 11:16:02 +0530

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -69,6 +69,7 @@ if ENV:
     URL = os.environ.get('URL', "")  # Does not contain token
     PORT = int(os.environ.get('PORT', 5000))
     CERT_PATH = os.environ.get("CERT_PATH")
+    REDIS_URL =os.environ.get('REDIS_URL')
     API_ID = os.environ.get('API_ID', None)
     API_HASH = os.environ.get('API_HASH', None)
     DB_URI = os.environ.get('DATABASE_URL')
