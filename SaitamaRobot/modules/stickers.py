3c4ece3a240935bd1807f5c6c768af54f61e5eda
Sasuke Uchiha <73424435+FtSasaki@users.noreply.github.com>
2020-12-19 15:09:28 +0530

    Update stickers.py

--- SaitamaRobot/modules/stickers.py
+++ SaitamaRobot/modules/stickers.py
@@ -153,19 +153,9 @@ def kang(update: Update, context: CallbackContext):
                     png_sticker=open("kangsticker.png", "rb"),
                     emojis=sticker_emoji,
                 )
-                kek_keyboard = InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton(
-                                text="View Pack ✨", url=f"t.me/addstickers/{packname}"
-                            )
-                        ]
-                    ]
-                    )
                 msg.reply_text(
                     f"Sticker successfully added to [pack](t.me/addstickers/{packname})"
                     + f"\nEmoji is: {sticker_emoji}",
-                    reply_markup=kek_keyboard,
                     parse_mode=ParseMode.MARKDOWN,
                 )
 
@@ -173,7 +163,7 @@ def kang(update: Update, context: CallbackContext):
                 msg.reply_text("I can only kang images m8.")
                 print(e)
                 return
-            
+
             except TelegramError as e:
                 if e.message == "Stickerset_invalid":
                     makepack_internal(
@@ -194,7 +184,7 @@ def kang(update: Update, context: CallbackContext):
                         png_sticker=open("kangsticker.png", "rb"),
                         emojis=sticker_emoji,
                     )
-                msg.reply_text(
+                    msg.reply_text(
                         f"Sticker successfully added to [pack](t.me/addstickers/{packname})"
                         + f"\nEmoji is: {sticker_emoji}",
                         parse_mode=ParseMode.MARKDOWN,
@@ -210,7 +200,7 @@ def kang(update: Update, context: CallbackContext):
                         % packname + "\n"
                         "Emoji is:" + " " + sticker_emoji,
                         parse_mode=ParseMode.MARKDOWN,
-                        )
+                    )
                 print(e)
 
         else:
@@ -237,21 +227,11 @@ def kang(update: Update, context: CallbackContext):
                     tgs_sticker=open("kangsticker.tgs", "rb"),
                     emojis=sticker_emoji,
                 )
-                 kek_keyboard = InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton(
-                                text="View Pack 🤙", url=f"t.me/addstickers/{packname}"
-                            )
-                        ]
-                    ]
-                    )
                 msg.reply_text(
                     f"Sticker successfully added to [pack](t.me/addstickers/{packname})"
                     + f"\nEmoji is: {sticker_emoji}",
-                    reply_markup=kek_keyboard,
                     parse_mode=ParseMode.MARKDOWN,
-                    )
+                )
             except TelegramError as e:
                 if e.message == "Stickerset_invalid":
                     makepack_internal(
@@ -311,21 +291,11 @@ def kang(update: Update, context: CallbackContext):
                 png_sticker=open("kangsticker.png", "rb"),
                 emojis=sticker_emoji,
             )
-             kek_keyboard = InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                        text="View Pack ⚡️", url=f"t.me/addstickers/{packname}"
-                          )
-                    ]
-                ]
-                )
             msg.reply_text(
                 f"Sticker successfully added to [pack](t.me/addstickers/{packname})"
                 + f"\nEmoji is: {sticker_emoji}",
-                reply_markup=kek_keyboard,
                 parse_mode=ParseMode.MARKDOWN,
-                )
+            )
         except OSError as e:
             msg.reply_text("I can only kang images m8.")
             print(e)
