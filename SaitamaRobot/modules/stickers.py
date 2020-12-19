930b853cfd906df2708da9bdb5287c24ff9b2da5
Sasuke Uchiha <73424435+FtSasaki@users.noreply.github.com>
2020-12-19 15:14:02 +0530

    Update stickers.py

--- SaitamaRobot/modules/stickers.py
+++ SaitamaRobot/modules/stickers.py
@@ -153,9 +153,19 @@ def kang(update: Update, context: CallbackContext):
                     png_sticker=open("kangsticker.png", "rb"),
                     emojis=sticker_emoji,
                 )
+                kek_keyboard = InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton(
+                                text="View Pack ✨", url=f"t.me/addstickers/{packname}"
+                            )
+                        ]
+                    ]
+                    )
                 msg.reply_text(
                     f"Sticker successfully added to [pack](t.me/addstickers/{packname})"
                     + f"\nEmoji is: {sticker_emoji}",
+                    reply_markup=kek_keyboard,
                     parse_mode=ParseMode.MARKDOWN,
                 )
 
@@ -227,9 +237,19 @@ def kang(update: Update, context: CallbackContext):
                     tgs_sticker=open("kangsticker.tgs", "rb"),
                     emojis=sticker_emoji,
                 )
+                kek_keyboard = InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton(
+                                text="View Pack ⚡️", url=f"t.me/addstickers/{packname}"
+                            )
+                        ]
+                    ]
+                    )
                 msg.reply_text(
                     f"Sticker successfully added to [pack](t.me/addstickers/{packname})"
                     + f"\nEmoji is: {sticker_emoji}",
+                    reply_markup=kek_keyboard,
                     parse_mode=ParseMode.MARKDOWN,
                 )
             except TelegramError as e:
