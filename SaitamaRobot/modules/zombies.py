139ff7f3c378767dec29824035284e2e4df77132
Sasuke Uchiha <73424435+FtSasaki@users.noreply.github.com>
2020-12-20 14:50:19 +0530

    Update zombies.py

--- SaitamaRobot/modules/zombies.py
+++ SaitamaRobot/modules/zombies.py
@@ -65,7 +65,7 @@ async def zombies(event):
 
     if con != "clean":
         find_zombies = await event.respond("Searching For Zombies...")
-        async for user in event.telethn.iter_participants(event.chat_id):
+        async for user in event.iter_participants(event.chat_id):
 
             if user.deleted:
                 del_u += 1
