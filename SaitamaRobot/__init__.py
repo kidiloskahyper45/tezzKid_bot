fb86b07abffec2992031f01e76b04a81cccf829c
Sasuke Uchiha <73424435+FtSasaki@users.noreply.github.com>
2020-12-28 02:02:45 +0530

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -196,7 +196,7 @@ else:
     sw = spamwatch.Client(SPAMWATCH_API)
 
 
-    updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
+updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 telethn = TelegramClient("saitama", API_ID, API_HASH)
 pbot = Client("senkuPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
