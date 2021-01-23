de85a497c6649bc0dd351eda744c0b1ff38f939c
KiDiLoSkA <73132025+kidiloskahyper45@users.noreply.github.com>
2021-01-23 21:16:12 +0530

    Update cleaner_sql.py

--- SaitamaRobot/modules/cleaner_sql.py
+++ SaitamaRobot/modules/cleaner_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean
 
-from cinderella.modules.sql import BASE, SESSION
+from SaitamaRobot.modules.sql import BASE, SESSION
 
 
 class CleanerBlueTextChatSettings(BASE):
