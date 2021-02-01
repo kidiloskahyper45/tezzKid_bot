a2a3807ee89b2cb0292ebb1b1be273d2f6384a44
KiDiLoSkA <73132025+kidiloskahyper45@users.noreply.github.com>
2021-02-02 01:08:23 +0530

    Update userinfo.py

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -226,7 +226,7 @@ def info(update: Update, context: CallbackContext):
     rep = message.reply_text(
         "<code>Appraising...</code>", parse_mode=ParseMode.HTML)
 
-    text = (f"╒═══「<b> Appraisal results:</b> 」\n"
+    text = (f"「<b> Telegram Information:</b> 」\n"
             f"ID: <code>{user.id}</code>\n"
             f"First Name: {html.escape(user.first_name)}")
 
